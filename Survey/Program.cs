7ff65241d2b34dd690b06e5ff6bedd89d1543575
bruh man 5th floor <davonta2222@gmail.com>
2021-05-31 17:02:15 -0700

    Add project files.

---
+++ Survey/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Survey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("what is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Your name is: {0}", name);
+
+            Console.WriteLine("How old are you?");
+            var age = Console.ReadLine();
+            Console.WriteLine("Your age is: {0}", age);
+
+            Console.WriteLine("What month are you born in?");
+            var month = Console.ReadLine();
+            Console.WriteLine("Your birth month is: {0}", month);
+        }
+    }
+}
